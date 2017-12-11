acdbbfd1ecddae9f71c1a4271af267ede0cedc78
Glenn Hickman <glenndrives@users.noreply.github.com>
2017-12-11 15:19:25 -0500

    Create driver-team-reset.ino
    Initial commit

---
+++ driver-team-reset.ino
@@ -0,0 +1,33 @@
+/*
+This project will take the button inputs from the TALLYH0 Photon and open a relay
+on the TALLYH1 Photon. This is to give Master Control remote reset capabilites
+for the TEAM unit at Driver.
+*/
+
+unsigned long lastTime;
+
+void setup() {
+
+Particle.subscribe("Studio", PingHandler, "370050001551353531343431");
+Particle.subscribe("team_reset", ResetTeam, "370050001551353531343431");
+}
+
+void loop() {
+/*    
+    if ((Time.now() - lastTime) > 120) {
+        Particle.publish("Driver", "online");
+        
+    lastTime = Time.now();
+    }
+*/
+}
+
+void PingHandler(const char *event, const char *data) {
+    Particle.publish("Driver", "pong");
+}
+
+void ResetTeam(const char *event, const char *data) {
+    if (String(data) == "reset") {
+        // Reset code here.
+    }
+}
